62a23716994f6617acadd577e48ae0990e8d411d
Diego Castro Oliveros <94019700+Diegocastro21@users.noreply.github.com>
2022-06-14 13:55:07 -0500

    subida testing Junit5

---
+++ maven-unit-test-junit5/src/test/java/io/code/maven/unittests/junit5/RunnerTest.java
@@ -0,0 +1,34 @@
+package io.code.maven.unittests.junit5;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RunnerTest {
+
+    @BeforeEach
+    public void setUp(){
+        System.out.println("\nRunning setUp. . . . .");
+    }
+
+    @Test
+    public void sumReturnsCorrectResult(){
+        System.out.println("Running sumReturnsCorrectResult. . . . .");
+
+        final int number1 = 3;
+        final int number2 = 5;
+
+        final int expected = 8;
+
+        final int actual = Runner.sum(number1, number2);
+
+        assertEquals(expected, actual);
+    }
+
+    @AfterEach
+    public void tearDown(){
+        System.out.println("Running tearDown. . . . .\n");
+    }
+}
